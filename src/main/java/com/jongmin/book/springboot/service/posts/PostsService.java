84aa7f806e04f1cc920dc7e5e500d38dbbc073b9
jongminshin <ace4463@gmail.com>
2020-02-01 00:13:28 +0900

    PostsApiController create & Test

---
+++ src/main/java/com/jongmin/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.jongmin.book.springboot.service.posts;
+
+import com.jongmin.book.springboot.domain.posts.PostsRepository;
+import com.jongmin.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
