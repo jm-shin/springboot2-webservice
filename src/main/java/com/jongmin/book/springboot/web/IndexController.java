6dd9583faf245faaad958b92b3c3424e4898dd4e
jongminshin <ace4463@gmail.com>
2020-02-06 00:15:02 +0900

    구글 로그인 구현

--- src/main/java/com/jongmin/book/springboot/web/IndexController.java
+++ src/main/java/com/jongmin/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jongmin.book.springboot.web;
 
+import com.jongmin.book.springboot.config.auth.dto.SessionUser;
 import com.jongmin.book.springboot.service.posts.PostsService;
 import com.jongmin.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
 
@@ -31,4 +39,5 @@ public class IndexController {
         model.addAttribute("post", dto);
         return "posts-update";
     }
+
 }
