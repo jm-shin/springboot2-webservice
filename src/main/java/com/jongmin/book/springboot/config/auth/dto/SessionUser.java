e0329b1e985a45e77877b8b86ab6c8495abd15c7
jongminshin <ace4463@gmail.com>
2020-02-07 13:57:21 +0900

    @LoginUser 어노테이션 생성

--- src/main/java/com/jongmin/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/jongmin/book/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.jongmin.book.springboot.config.auth.dto;
 import com.jongmin.book.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
--- src/main/java/com/jongmin/book/springboot/web/IndexController.java
+++ src/main/java/com/jongmin/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jongmin.book.springboot.web;
 
+import com.jongmin.book.springboot.config.auth.LoginUser;
 import com.jongmin.book.springboot.config.auth.dto.SessionUser;
 import com.jongmin.book.springboot.service.posts.PostsService;
 import com.jongmin.book.springboot.web.dto.PostsResponseDto;
@@ -11,19 +12,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpSession;
 
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if (user != null){
-            model.addAttribute("userName",user.getName());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
         }
         return "index";
     }
